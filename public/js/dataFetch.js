2023814c97a884cba0c68e4501cc99a4c1d9af72
leerlandais <leerlandais@gmail.com>
2024-10-15 19:33:50 +0200

    added test json - all ok

--- public/js/dataFetch.js
+++ public/js/dataFetch.js
@@ -1,13 +1,23 @@
- console.log("dataFetch connected"); // This works
+ // console.log("dataFetch connected"); // This works
 
 function getDatas() {
-    fetch("scripts/dummy-datas.json")
+    fetch("js/dummy-datas.json")
         .then(function(response) {
             return response.json();
         })
         .then(function(datas) {
+            //       console.log('Fetched data:', datas); // Check if data is fetched correctly
             postDatas(datas);
         })
         .catch(error => console.error('Error fetching datas:', error));
 }
+
+
+function postDatas(datas) {
+
+// console.log(datas); // this works, too
+}
+
+
+getDatas();
 
