28e8606e8736bf4b10a54f9e8449619c981eb11b
leerlandais <leerlandais@gmail.com>
2024-10-15 19:21:43 +0200

    added basic fetch script

--- public/js/dataFetch.js
+++ public/js/dataFetch.js
@@ -1,1 +1,13 @@
  console.log("dataFetch connected"); // This works
+
+function getDatas() {
+    fetch("scripts/dummy-datas.json")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(datas) {
+            postDatas(datas);
+        })
+        .catch(error => console.error('Error fetching datas:', error));
+}
+
