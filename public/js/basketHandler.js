bbc37b6538f404e17bb8c2443a1df26405c88655
leerlandais <leerlandais@gmail.com>
2024-10-17 11:01:58 +0200

    breaktime

--- public/js/basketHandler.js
+++ public/js/basketHandler.js
@@ -1,10 +1,13 @@
 
 function addToBasket(added) {
+
+
+
+   /*
    let sellThis = JSON.parse(localStorage.getItem(added));
    console.log(sellThis);
    sellThis.sold = (Number(sellThis.sold) + 1);
    localStorage.setItem(added, JSON.stringify(sellThis));
    console.log(sellThis);
-
-
+*/
 }
