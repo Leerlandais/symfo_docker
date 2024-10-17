0a679b23d2f86f46060d8369c4bc6cff1229cf89
leerlandais <leerlandais@gmail.com>
2024-10-17 10:58:15 +0200

    randomised amounts sold for localFiller

--- public/js/localStorageFiller.js
+++ public/js/localStorageFiller.js
@@ -1,6 +1,6 @@
 
 
 for(let i = 1; i <= 12; i++) {
-    const item = {id: i, sold: 0};
+    const item = {id: i, sold: Math.floor(Math.random() * 20) + 1};
     localStorage.setItem('ITEM'+i, JSON.stringify(item));
 }
